f2e5c48cf7478e5e3418609ee002d2482928e45c
Hao Wu <hwuu@outlook.com>
2023-03-22 20:26:11 +0800

    Refine the whole workflow. (#3)

--- spk2ai.py
+++ spk2ai.py
@@ -1,4 +1,5 @@
 import os
+import re
 import json
 import argparse
 
@@ -20,49 +21,59 @@ speech_config = speechsdk.SpeechConfig(subscription=config["speech_key"], region
 speech_config.speech_recognition_language=config["language"]
 speech_config.speech_synthesis_language = config["language"]
 speech_config.speech_synthesis_voice_name = "%s-%sNeural" % (config["language"], config["voice"])
-#speech_config.set_property(speechsdk.PropertyId.Speech_ServiceProperties_Voice_Speed, "1.5")
 
 speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
 speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
 
-history = []
-to_exit = False
+#
 
+print("Please start speaking. You can say 'stop' to intercept the AI and say 'exit' to return to your real, yet boring, world.\n")
+
+history = []
 while True:
-    print("Please start speaking...")
+    print(f"[{len(history)}] Me: ", end="", flush=True)
     result = speech_recognizer.recognize_once_async().get()
     input_text = result.text
-    print("[@me] " + input_text + "\n")
+    print(input_text + "\n")
 
-    if input_text[:-2].endswith("Exit"):
-        input_text = "That's all for today. Bye."
-        to_exit = True
+    response_text = ""
+    cmd = re.sub(r'[^a-zA-Z]', ' ', input_text).strip().lower()
+    if cmd == "stop":
+        speech_synthesizer.stop_speaking()
+        if config["language"].split("-")[0] == "zh":
+            response_text = "好的, 请你先说."
+        elif config["language"].split("-")[0] == "en":
+            response_text = "OK. I'm listening."
+    elif cmd == "exit":
+        if config["language"].split("-")[0] == "zh":
+            response_text = "好的, 再见."
+        elif config["language"].split("-")[0] == "en":
+            response_text = "OK. See you next time."
+    else:
+        messages = []
+        for x, y in history:
+            messages.append({"role": "user", "content": x})
+            messages.append({"role": "assistant", "content": y})
+        messages.append({"role": "user", "content": input_text})
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            temperature=0.3,
+            #max_tokens=4096,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+        )
+        response_text = response.choices[0].message.content
+        history.append((input_text, response_text))
 
-    messages = []
-    for x, y in history[-20:]:
-        messages.append({"role": "user", "content": x})
-        messages.append({"role": "assistant", "content": y})
-    messages.append({"role": "user", "content": input_text})
+    print(f"[{len(history)}] AI: " + response_text.strip() + "\n")
+    f = speech_synthesizer.speak_text_async(response_text)
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages,
-        temperature=0.3,
-        #max_tokens=4096,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-    )
-    response_text = response.choices[0].message.content
-    print("[@AI] " + response_text.strip() + "\n")
-
-    history.append((input_text, response_text))
-    if len(history) > 100:
-        history.pop(0)
-
-    speech_synthesizer.speak_text_async(response_text).get()
-
-    if to_exit:
+    if cmd == "stop":
+        f.get()
+    elif cmd == "exit":
+        f.get()
         break
 
 #
